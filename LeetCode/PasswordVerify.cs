44c896400ccca6c3fe7014aad0df562ef95a080f
sanyambhatia01 <sanyam.bhatia@gokloud.io>
2022-05-19 22:31:41 +0530

    PasswordVerifier

---
+++ LeetCode/PasswordVerify.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordVerifier
+{
+    public class PasswordVerify
+    {
+        static bool VerifyPassword(string input)
+        {
+            bool num = false;
+            bool upp = false;
+            bool low = false;
+            bool spec = false;
+            char currentChar;
+            if (!(input.Length >= 2))
+            {
+                return false;
+                throw new ArgumentException("Length of the Password is must be greater then 2");
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentChar = input[i];
+                if (char.IsDigit(currentChar))
+                {
+                    num = true;
+                }
+                else if (char.IsUpper(currentChar))
+                {
+                    upp = true;
+                }
+                else if (char.IsLower(currentChar))
+                {
+                    low = true;
+                }
+                else if (!char.IsLetterOrDigit(currentChar))
+                {
+                    spec = true;
+                }
+
+                if (num && upp && low && spec)
+                {
+                    return true;
+                }
+                throw new ArgumentException("password not valid");
+            }
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+                throw new ArgumentException("Empty Password");
+            }
+            return false;
+        }
+        public static bool PasswordCheck(string input)
+        {
+            return VerifyPassword(input);
+        }
+    }
+}
---
+++ UnitTest/PasswordVerifierTest.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode;
+using PasswordVerifier;
+
+namespace PasswordVerifierTest
+{
+    public class PasswordTest
+    {
+        [Theory]
+        [InlineData("oausgdsa")]
+        [InlineData("Passw0rd12!@")]
+        [InlineData("appleball123")]
+        [InlineData("Abc@123")]
+        [InlineData("oongaboonga2323")]
+        [InlineData("233@%$aBa")]
+        [InlineData("app1#$3B")]
+        [InlineData("dkjcnskdcn12@#$23P")]
+        [InlineData("tajikistan66")]
+        [InlineData("Sanyam@098")]
+        [InlineData("france")]
+        [InlineData("APPLE1PAD")]
+        [InlineData("dmclscAC@%^6")]
+        [InlineData("bnGj920&%")]
+        [InlineData("Delhi@#00")]
+        [InlineData("   ")]
+        [InlineData("astdvhj")]
+        public void Password_IsValid_ThrowException(string input)
+        {
+            //arrange
+            PasswordVerify passwordVerify = new PasswordVerify();
+            // act & assert
+            Assert.Throws<ArgumentException>(() => PasswordVerify.PasswordCheck(input));
+        }
+
+    }
+}
